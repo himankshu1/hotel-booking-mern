81d5101a6fd85a4c178897ab643e598faf7fba95
Himanshu Kumar <himankshu1@gmail.com>
2024-05-13 16:06:40 +0530

    implemented & tested react router

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,28 +1,34 @@
 import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
 
 const Header = () => {
   return (
-    <div className="bg-blue-800 py-6">
-      <div className="container mx-auto flex justify-between items-center">
+    <div className="py-6 bg-emerald-950">
+      <div className="container mx-auto px-8 flex justify-between items-center">
         {/* logo */}
-        <div className="flex">
+        <div className="flex items-center cursor-pointer gap-1">
           <div>
             <img src={logo} alt="logo" />
           </div>
-          <span className="text-emerald-500 font-semibold tracking-tight">
-            <Link to="/">Nest.</Link>
+          <span className="">
+            <Link
+              to="/"
+              className="text-emerald-500 font-semibold tracking-tight text-2xl"
+            >
+              Nest.
+            </Link>
           </span>
         </div>
 
         {/* login button */}
         <span className="flex space-x-2">
-          <Link
-            to="/sign-in"
-            className="flex items-center text-emerald-500 px-3 font-bold hover:cursor-pointer hover:text-emerald-100"
+          <Button
+            variant="outline"
+            className="flex items-center text-emerald-500 px-3 font-bold hover:cursor-pointer hover:bg-emerald-500 hover:text-white"
           >
             Sign In
-          </Link>
+          </Button>
         </span>
       </div>
     </div>
---
+++ frontend/src/router.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "./layouts/RootLayout";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/sign-in",
+        element: <h1>Login Page</h1>,
+      },
+      {
+        path: "/register",
+        element: <h1>Register </h1>,
+      },
+    ],
+  },
+]);
