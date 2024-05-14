bc79d9b2f480099a6d32bbfb7c9b0ad517ea6e9b
Himanshu Kumar <himankshu1@gmail.com>
2024-05-14 14:50:37 +0530

    user controller jwt, cookie setup

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,7 +1,15 @@
 import { Request, Response } from "express";
 import { User } from "../models/user.model";
+import jwt from "jsonwebtoken";
 
+// user registration
 const registerUser = async (req: Request, res: Response) => {
+  // get user data from request
+  // check if the user exists and return
+  // create the user
+  // create token
+  // send to cookie
+  // return the response
   try {
     console.log(req.body);
     let user = await User.findOne({ email: req.body.email });
@@ -15,6 +23,22 @@ const registerUser = async (req: Request, res: Response) => {
 
     user = new User(req.body);
     await user.save();
+
+    const token = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_SECRET_KEY as string,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    res.cookie("auth_token", token, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 86400000,
+    });
+
+    return res.sendStatus(200);
   } catch (error) {
     console.log("Error while registering the user :: ", error);
     res.status(500).json({ message: "something went wrong!" });
