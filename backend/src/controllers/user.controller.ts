fe664fb3b9dedc93f3d27df34114e0df45311e0c
Himanshu Kumar <himankshu1@gmail.com>
2024-05-14 23:51:30 +0530

    user signin & controller

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { User } from "../models/user.model";
 import jwt from "jsonwebtoken";
 import { validationResult } from "express-validator";
+import bcrypt, { hash } from "bcrypt";
 
 // user registration
 const registerUser = async (req: Request, res: Response) => {
@@ -53,6 +54,56 @@ const registerUser = async (req: Request, res: Response) => {
 };
 
 // user login
-const signInUser = async () => {};
+const signInUser = async (req: Request, res: Response) => {
+  // check if there is any error in express validator
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ message: errors.array() });
+  }
+
+  // get user data
+  const { email, password } = req.body;
+
+  try {
+    // check for the user in the db
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "invalid credentials" });
+    }
+
+    // compare & match the password using bcrypt
+    const isPasswordMatched = bcrypt.compare(password, user.password);
+    if (!isPasswordMatched) {
+      return res.status(400).json({ message: "invalid credentials" });
+    }
+
+    // generate an access token
+    const token = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_SECRET_KEY as string,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    // setting up the cookie
+    res.cookie("auth_token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 86400000,
+    });
+
+    // sending the response
+    res
+      .status(200)
+      .json({ message: "logged in successfully", userId: user._id });
+  } catch (error) {
+    console.log("Something went wrong while signing in :: ", error);
+    return res
+      .status(500)
+      .json({ message: "Something went wrong while signing in" });
+  }
+};
 
 export { registerUser, signInUser };
